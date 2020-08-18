304d9934e7b98a030eacb0e60c9cfd3282ae7008
Chris Salgado <musicman785@gmail.com>
2020-08-18 07:34:57 -0700

    Cretes simple contact page with link to email

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/navbar";
 import Portfolio from "./pages/Portfolio/porfolio";
 import About from "./pages/About/About"
+import Contact from "./pages/Contact/Contact"
 
 
 const App = () => {
@@ -15,6 +16,7 @@ const App = () => {
         <Route exact path="/" component={Portfolio} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
       </div>
     </Router>
 
---
+++ portfolio/src/pages/Contact/Contact.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Hero from "../../components/Hero/hero"
+import Footer from "../../components/Footer/Footer"
+
+
+
+const Contact = () => {
+    return (
+        <div>
+            <Hero backgroundImage="https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80">
+                <h1>Lets get in touch!</h1>
+            </Hero>
+
+            <div style={{ textAlign: "center" }}><a className="btn btn-block btn-lg btn-dark white" href="https://www.gmail.com/" target="_blank" rel="noopener noreferrer">Click Here!</a></div>
+
+            <Footer />
+        </div>
+    )
+}
+
+export default Contact;
